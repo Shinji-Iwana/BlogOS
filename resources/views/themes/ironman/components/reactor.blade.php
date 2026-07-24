77711bf00577f80ec1f3a4a07fd7c0f7beaaa3af
Shinji-Iwana <shin.rock.m.2115@gmail.com>
2026-07-24 16:16:47 +0900

    feat: ironmanテーマをファイル分割

--- resources/views/themes/ironman/components/reactor.blade.php
+++ resources/views/themes/ironman/components/reactor.blade.php
@@ -2,33 +2,78 @@
 
     <div class="reactor-core">
 
-        <div class="reactor-ring ring1">
-            <span class="energy e1"></span>
-            <span class="energy e2"></span>
-            <span class="energy e3"></span>
-            <span class="energy e4"></span>
-            <span class="energy e5"></span>
+        <!-- ==========================
+             Frame Layer
+        =========================== -->
+
+        <div class="layer-frame">
+
+    <div class="support-frame">
+
+    <div class="support-module module-top"></div>
+
+    <div class="support-module module-right"></div>
+
+    <div class="support-module module-bottom"></div>
+
+    <div class="support-module module-left"></div>
+
+</div>
+
+</div>
+
+        <!-- ==========================
+             Ring Layer
+        =========================== -->
+
+        <div class="layer-rings">
+
+            <div class="reactor-ring ring1">
+                <span class="energy e1"></span>
+                <span class="energy e2"></span>
+                <span class="energy e3"></span>
+                <span class="energy e4"></span>
+                <span class="energy e5"></span>
+            </div>
+
+            <div class="reactor-ring ring2">
+                <span class="energy e1"></span>
+                <span class="energy e2"></span>
+                <span class="energy e3"></span>
+                <span class="energy e4"></span>
+                <span class="energy e5"></span>
+            </div>
+
+            <div class="reactor-ring ring3">
+                <span class="energy e1"></span>
+                <span class="energy e2"></span>
+                <span class="energy e3"></span>
+                <span class="energy e4"></span>
+                <span class="energy e5"></span>
+            </div>
+
         </div>
 
-        <div class="reactor-ring ring2">
-            <span class="energy e1"></span>
-            <span class="energy e2"></span>
-            <span class="energy e3"></span>
-            <span class="energy e4"></span>
-            <span class="energy e5"></span>
+        <!-- ==========================
+             Core Layer
+        =========================== -->
+
+        <div class="layer-core">
+
+            <img
+                src="/themes/ironman/images/arc-reactor.png"
+                class="arc-reactor"
+                alt="Arc Reactor">
+
         </div>
 
-        <div class="reactor-ring ring3">
-            <span class="energy e1"></span>
-            <span class="energy e2"></span>
-            <span class="energy e3"></span>
-            <span class="energy e4"></span>
-            <span class="energy e5"></span>
-        </div>
+        <!-- ==========================
+             Effect Layer
+        =========================== -->
 
-        <img src="/themes/ironman/images/arc-reactor.png"
-             class="arc-reactor"
-             alt="Arc Reactor">
+        <div class="layer-effects">
+
+        </div>
 
     </div>
 
